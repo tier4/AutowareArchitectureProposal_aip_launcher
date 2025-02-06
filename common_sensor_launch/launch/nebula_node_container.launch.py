f8678425e0221f893d0456c02ac9e33c66def8cb
Shintaro Sakoda <shintaro.sakoda@tier4.jp>
2025-02-06 11:08:19 +0900

    Added ptp_lock_threshold
    Signed-off-by: Shintaro Sakoda <shintaro.sakoda@tier4.jp>

--- common_sensor_launch/launch/nebula_node_container.launch.py
+++ common_sensor_launch/launch/nebula_node_container.launch.py
@@ -275,6 +275,7 @@ def launch_setup(context, *args, **kwargs):
                     "ptp_transport_type",
                     "ptp_switch_type",
                     "ptp_domain",
+                    "ptp_lock_threshold",
                     "retry_hw",
                 ),
             }
@@ -360,6 +361,7 @@ def generate_launch_description():
     add_launch_arg("ptp_transport_type", "L2")
     add_launch_arg("ptp_switch_type", "TSN")
     add_launch_arg("ptp_domain", "0")
+    add_launch_arg("ptp_lock_threshold", "100")
     add_launch_arg("output_as_sensor_frame", "True", "output final pointcloud in sensor frame")
     add_launch_arg("enable_blockage_diag", "true")
     add_launch_arg("horizontal_ring_id", "64")
